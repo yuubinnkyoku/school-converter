f0f68f9e2ba124f76d1008b078e24e31c0b34a96
yuubinnkyoku <yuubinnkyokuda0730@gmail.com>
2025-08-06 13:44:15 +0900

    tailwind init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui']
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  css: ['~/assets/css/main.css']
 })
